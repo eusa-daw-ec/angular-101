381f22a96077502ba8c1b828dc2bda825d295d57
eusa-daw-ec <147559236+eusa-daw-ec@users.noreply.github.com>
2024-01-23 20:11:13 +0000

    Segunda parte

---
+++ mystore/src/app/alerta-producto/alerta-producto.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { datosProductos } from '../datosProductos';
+import { Producto } from '../productosInterface';
+
+
+@Component({
+  selector: 'app-alerta-producto',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alerta-producto.component.html',
+  styleUrl: './alerta-producto.component.css'
+})
+export class AlertaProductoComponent {
+
+  @Input() producto!: Producto;
+  @Output() aviso = new EventEmitter();
+
+
+}
--- mystore/src/app/lista-productos/lista-productos.component.ts
+++ mystore/src/app/lista-productos/lista-productos.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { datosProductos } from '../datosProductos'
 import { CommonModule } from '@angular/common';
+import { AlertaProductoComponent } from '../alerta-producto/alerta-producto.component';
 
 @Component({
   selector: 'app-lista-productos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AlertaProductoComponent],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
 export class ListaProductosComponent {
 
   productos = datosProductos;
+
+  compartir() {
+    window.alert("El producto se ha compartido!!")
+  }
+
+  onAviso() {
+    window.alert('Te avisaremos cuando el producto salga a la venta...');
+  }
 
 }
