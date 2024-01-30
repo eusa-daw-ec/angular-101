fd3afa8d66bb13246a6dff88ece0796d83424256
eusa-daw-ec <147559236+eusa-daw-ec@users.noreply.github.com>
2024-01-30 20:27:11 +0000

    primer commit

---
+++ routing/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { DetalleCursoComponent } from './detalle-curso/detalle-curso.component';
+
+export const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'contacto', component: ContactoComponent },
+    { path: 'acercade', component: AcercadeComponent },
+    { path: 'cursos', component: CursosComponent },
+    { path: 'cursos/:curso', component: DetalleCursoComponent },
+];
---
+++ routing/src/app/detalle-curso/detalle-curso.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalle-curso',
+  standalone: true,
+  imports: [ActivatedRoute],
+  providers: [],
+  templateUrl: './detalle-curso.component.html',
+  styleUrl: './detalle-curso.component.css'
+})
+export class DetalleCursoComponent {
+
+}
