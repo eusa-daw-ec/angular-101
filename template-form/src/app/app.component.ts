b916072ecd8c3a59dae1c67308552a2fd2dbd9f6
eusa-daw-ec <147559236+eusa-daw-ec@users.noreply.github.com>
2024-01-31 22:47:02 +0000

    Actualización template-forms

---
+++ template-form/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TemplateFormComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'form-plantilla';
+}
